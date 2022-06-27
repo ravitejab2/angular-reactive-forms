5b379bd54ec4e9573da7e6706a0e3796a0543248
ravitejabollineni555@gmail.com <ravitejabollineni555@gmail.com>
2022-06-27 16:59:24 +0530

    added logout functionality  in admin component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:AuthService,private router:Router) { }
 
   ngOnInit(): void {
+  }
+  logout(){
+    this.service.logout();
+    this.router.navigateByUrl("/login");
   }
 
 }
